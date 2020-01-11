e81adc35bfde0431a5c7cb5a5749e1d91d1924b7
SalahP <hs.salah@outlook.fr>
2020-01-11 23:50:21 +0100

    frontend

--- FastCoinNetwork/src/React/Tuto/Tuto6.js
+++ FastCoinNetwork/src/React/Tuto/Tuto6.js
@@ -7,7 +7,7 @@ class Tuto6 extends React.Component {
     return (
         <div class="defil text-center mt-5">
             <h6 class="mt-1 mb-3">Vous souhaitez retirer vos deniers:</h6>
-            <li>En étant connecté au réseau Fast Coin, envoyer le montant souhaité vers l'adresse: 0x79FADb528702a9C881389fE6Df1B37f03ACa0e90</li>
+            <li>En étant connecté au réseau Fast Coin, envoyer le montant souhaité vers l'adresse: 0x659432ab0e6324Df1dE5e1f5D2D664CECA6cEb69</li>
             <li>Depuis votre espace, générez votre message secret</li>
             <li>Connectez-vous au réseau Ropsten et demandez un retrait en y indiquant le montant correspondant ainsi que le message secret</li>
             <p class="mt-2">Votre retrait s'est réalisé avec succès!</p>
